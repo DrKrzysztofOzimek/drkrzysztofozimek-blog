d1d924c1004bb891e04612bd9c07475ac6b0d778
Dr Krzysztof Ozimek <contact@drkrzysztofozimek.com>
2025-05-29 13:26:07 +0200

    Delete gpower_sample_size_python.py

--- gpower_sample_size_python.py
+++
@@ -1,7 +0,0 @@
-from statsmodels.stats.power import TTestPower
-
-analysis = TTestPower()
-sample_size = analysis.solve_power(
-    effect_size=0.6, power=0.9, alpha=0.05, alternative="larger"
-)
-sample_size
