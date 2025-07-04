4ae91808d1a217ec894bdc540b6fd2b06012110f
Dr Krzysztof Ozimek <contact@drkrzysztofozimek.com>
2025-07-04 14:39:12 +0200

    Add files via upload

---
+++ numerical-hedging-portfolio-risk/hedging.R
@@ -0,0 +1,21 @@
+hedge_coefficients <- function(Pi, x0, h) {
+  # Central differences
+  Pi_plus <- Pi(x0 + h)
+  Pi_minus <- Pi(x0 - h)
+  Pi_0 <- Pi(x0)
+
+  # Derivatives
+  dPi_dx <- (Pi_plus - Pi_minus) / (2 * h)
+  d2Pi_dx2 <- (Pi_plus - 2 * Pi_0 + Pi_minus) / (h^2)
+
+  # Hedging coefficients
+  m <- -0.5 * d2Pi_dx2
+  n <- -dPi_dx - 2 * m * x0
+
+
+  return(list(n = n, m = m))
+}
+
+# Example usage
+Pi <- function(x) 0.5 * x^2 + 2 * x + 5
+hedge_coefficients(Pi, x0 = 5, h = 0.01)
